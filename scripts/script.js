e0dd7d64a59b0a66bd7055069fa7d47586ef9305
mariam-85 <mariam.lofink85@gmail.com>
2022-09-26 12:39:04 +0200

    changes added

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,101 @@
 
 const form = document.querySelector('.form');
 const container = document.querySelector('.container');
+let cards_info = [];
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+// const render = () => {
+//     container.innerText = '';
+//     cards_info.forEach(({ name, last_name, age, rate, days, email, photo, progress }) => {
+//         const card = document.createElement('div');
+//   const nameElem = document.createElement('p');
+//   const lastNameElem = document.createElement('p');
+//   const ageElem = document.createElement('p');
+//   const salaryElem = document.createElement('p');
+//   const photoElem = document.createElement('img');
+//   const emailElem = document.createElement('a');
+//   const emailTextElem = document.createElement('span');
+//   const progressContainer = document.createElement('div');
+//   const progressLine = document.createElement('div');
+//   const progressValue = document.createElement('p');
+//   const emailContainer = document.createElement('div');
 
-  const card = document.createElement('div');
+
+//   nameElem.innerText = `Name: ${name}`;
+//   lastNameElem.innerText = `Last name: ${last_name}`;
+//   ageElem.innerText = `Age: ${age}`;
+//   salaryElem.innerText = `Salary: ${rate * days}`;
+//   emailTextElem.innerText = 'Email: ';
+//   emailElem.innerText = `${email}`;
+//   progressValue.innerText = `${progress}%`;
+
+//   photoElem.setAttribute('src', photo);
+//   photoElem.setAttribute('alt', 'photo');
+//   emailElem.setAttribute('href', `mailto:${email}`);
+
+
+//   card.classList.add('card');
+
+//   photoElem.classList.add('img'); 
+
+//   emailElem.classList.add('email');
+  
+
+//   progressContainer.classList.add('progress-container');
+  
+//   progressLine.classList.add('progress-line');
+
+//   progressValue.classList.add('progress-value');
+
+//   progressLine.style.width = progress + '%';
+
+//   progressLine.style.backgroundColor = progress >= 50 ? 'powderblue' : 'azure';
+
+
+//   progressContainer.append(progressLine);
+
+//   progressLine.append(progressValue);
+
+//   emailContainer.append(emailTextElem, emailElem);
+
+
+//   card.append(nameElem, lastNameElem, ageElem, salaryElem, photoElem, emailContainer, progressContainer);
+//   container.append(card);
+
+//     })
+// }
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+
+//     const{ name, last_name, age, rate, days, photo, email, progress } = event.target;
+
+//     cards_info.push({
+//         name:name.value,
+//         last_name:last_name.value,
+//         age:age.value,
+//         rate:rate.value,
+//         days:days.value,
+//         email:email.value,
+//         photo:photo.value,
+//         progress:progress.value
+
+//     })
+
+//     name.value = '';
+//     last_name.value = '';
+//     age.value = '';
+//     rate.value = '';
+//     days.value = '';
+//     photo.value = '';
+//     email.value = '';
+//     progress.value = '';
+   
+//     render();
+
+// })
+
+const render = ({ name, last_name, age, rate, days, email, photo, progress }) => {
+        const card = document.createElement('div');
   const nameElem = document.createElement('p');
   const lastNameElem = document.createElement('p');
   const ageElem = document.createElement('p');
@@ -23,21 +113,18 @@ form.addEventListener('submit', (event) => {
   const progressValue = document.createElement('p');
   const emailContainer = document.createElement('div');
 
-  
 
-  const{ name, last_name, age, rate, days, photo, email, progress } = event.target;//this
-
-  nameElem.innerText = `Name: ${name.value}`;
-  lastNameElem.innerText = `Last name: ${last_name.value}`;
-  ageElem.innerText = `Age: ${age.value}`;
-  salaryElem.innerText = `Salary: ${rate.value * days.value}`;
+  nameElem.innerText = `Name: ${name}`;
+  lastNameElem.innerText = `Last name: ${last_name}`;
+  ageElem.innerText = `Age: ${age}`;
+  salaryElem.innerText = `Salary: ${rate * days}`;
   emailTextElem.innerText = 'Email: ';
-  emailElem.innerText = `${email.value}`;
-  progressValue.innerText = `${progress.value}%`;
+  emailElem.innerText = `${email}`;
+  progressValue.innerText = `${progress}%`;
 
-  photoElem.setAttribute('src', photo.value);
+  photoElem.setAttribute('src', photo);
   photoElem.setAttribute('alt', 'photo');
-  emailElem.setAttribute('href', `mailto:&{email.value}`);
+  emailElem.setAttribute('href', `mailto:${email}`);
 
 
   card.classList.add('card');
@@ -53,9 +140,9 @@ form.addEventListener('submit', (event) => {
 
   progressValue.classList.add('progress-value');
 
-  progressLine.style.width = progress.value + '%';
+  progressLine.style.width = progress + '%';
 
-  progressLine.style.backgroundColor = progress.value >= 50 ? 'powderblue' : 'azure';
+  progressLine.style.backgroundColor = progress >= 50 ? 'powderblue' : 'azure';
 
 
   progressContainer.append(progressLine);
@@ -68,14 +155,35 @@ form.addEventListener('submit', (event) => {
   card.append(nameElem, lastNameElem, ageElem, salaryElem, photoElem, emailContainer, progressContainer);
   container.append(card);
 
-  name.value = '';
-  last_name.value = '';
-  age.value = '';
-  rate.value = '';
-  days.value = '';
-  photo.value = '';
-  email.value = '';
-  progress.value = '';
+    }
 
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const{ name, last_name, age, rate, days, photo, email, progress } = event.target;
+
+    cards_info.push({
+        name:name.value,
+        last_name:last_name.value,
+        age:age.value,
+        rate:rate.value,
+        days:days.value,
+        email:email.value,
+        photo:photo.value,
+        progress:progress.value
+
+    })
+
+    name.value = '';
+    last_name.value = '';
+    age.value = '';
+    rate.value = '';
+    days.value = '';
+    photo.value = '';
+    email.value = '';
+    progress.value = '';
+   
+    render(cards_info[cards_info.length - 1]);
 
 })
